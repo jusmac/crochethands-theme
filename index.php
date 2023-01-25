b4e05fac54120ad0f58e4006e1070b8d966d3f36
jusmac <justyna.machalica@gmail.com>
2023-01-25 09:47:23 +0100

    1st

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
+
+
+<?php include('header.php'); ?>
+
+  <body>
+
+    <main>
+
+      <h1></h1>
+
+      <p></p>
+
+    </main>
+
+    <?php include('footer.php'); ?>
+
+  </body>
+
+</html>
